0bd5ae3e5200680fdf42b20c139e1642d15fa1c3
unknown <joshharm@yahoo.com>
2019-01-19 16:57:00 -0600

    validations and gui nearing completion

--- src/src/com/harm/solutions/GUI/OBD_Scanner_Driver.java
+++ src/src/com/harm/solutions/GUI/OBD_Scanner_Driver.java
@@ -16,6 +16,8 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.net.MalformedURLException;
 import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 import javax.swing.JButton;
@@ -29,21 +31,26 @@ import javax.xml.bind.Unmarshaller;
 
 import src.com.harm.solutions.PDFGenerator.PDFGenerator;
 import src.com.harm.solutions.models.Doc;
+import src.com.harm.solutions.utilities.ProcessingException;
 
 public class OBD_Scanner_Driver {
 
 	private JFrame frame;
-	private JTextField textField;
+	private JTextField companyName;
 
 	private Map xmlDiagnosticContent;
 	private PDFGenerator generator;
 
 	Doc rawData;
-	private JTextField textField_1;
-	private JTextField textField_2;
-	private JTextField textField_3;
-	private JTextField textField_4;
-	private JTextField textField_5;
+	private JTextField companyStreetAddress;
+	private JTextField companyCityStateZip;
+	private JTextField companyPhoneNumber;
+	private JTextField invoiceNumber;
+	private JTextField invoicePayableDate;
+	private JTextField customerFullName;
+	private JTextField customerStreetAddress;
+	private JTextField customerCityStateZip;
+	private JTextField customerPhoneNumber;
 
 	/**
 	 * Launch the application.
@@ -77,7 +84,7 @@ public class OBD_Scanner_Driver {
 	 */
 	private void initialize() {
 		frame = new JFrame();
-		frame.setBounds(100, 100, 490, 729);
+		frame.setBounds(100, 100, 404, 729);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
 
@@ -85,40 +92,17 @@ public class OBD_Scanner_Driver {
 		label1.setFont(new Font("Arial", Font.BOLD, 20));
 		label1.setBounds(70, 28, 288, 22);
 		frame.getContentPane().add(label1);
-
-		JButton createPDFButton = new JButton("Create PDF Invoice");
-		createPDFButton.setBounds(97, 619, 261, 23);
-
-		createPDFButton.addActionListener(new ActionListener() {
-
-			@Override
-			public void actionPerformed(ActionEvent arg0) {
-				validateSubmission();
-			}
-
-			private void validateSubmission() {
-				// TODO Auto-generated method stub
-				
-			}
-
-		});
-
-		frame.getContentPane().add(createPDFButton);
-
-		Label label = new Label("Company Name to Use: ");
-		label.setBounds(28, 166, 154, 22);
-		frame.getContentPane().add(label);
-
-		textField = new JTextField();
-		textField.setBounds(182, 168, 176, 20);
-		frame.getContentPane().add(textField);
-		textField.setColumns(10);
+		companyName = new JTextField();
+		companyName.setName("companyName");
+		companyName.setBounds(163, 112, 176, 20);
+		frame.getContentPane().add(companyName);
+		companyName.setColumns(10);
 
 		JLabel fileSelectedLabel = new JLabel("No File Selected Yet");
 		fileSelectedLabel.setBackground(Color.GREEN);
 		fileSelectedLabel.setHorizontalAlignment(SwingConstants.CENTER);
 		fileSelectedLabel.setFont(new Font("Sitka Small", Font.PLAIN, 13));
-		fileSelectedLabel.setBounds(97, 492, 261, 14);
+		fileSelectedLabel.setBounds(47, 560, 261, 14);
 		frame.getContentPane().add(fileSelectedLabel);
 
 		JFileChooser chooser = new JFileChooser();
@@ -184,53 +168,177 @@ public class OBD_Scanner_Driver {
 
 			}
 		});
-		btnNewButton_1.setBounds(97, 521, 261, 23);
+		btnNewButton_1.setBounds(28, 585, 303, 23);
 		frame.getContentPane().add(btnNewButton_1);
-		
+
 		JLabel lblNewLabel = new JLabel("Street Address: ");
-		lblNewLabel.setBounds(28, 202, 144, 14);
+		lblNewLabel.setBounds(28, 143, 144, 14);
 		frame.getContentPane().add(lblNewLabel);
-		
-		textField_1 = new JTextField();
-		textField_1.setBounds(182, 199, 176, 20);
-		frame.getContentPane().add(textField_1);
-		textField_1.setColumns(10);
-		
+
+		companyStreetAddress = new JTextField();
+		companyStreetAddress.setName("companyStreetAddress");
+		companyStreetAddress.setBounds(163, 140, 176, 20);
+		frame.getContentPane().add(companyStreetAddress);
+		companyStreetAddress.setColumns(10);
+
 		JLabel lblCityStZip = new JLabel("City, ST ZIP Code: ");
-		lblCityStZip.setBounds(28, 237, 154, 14);
+		lblCityStZip.setBounds(28, 177, 154, 14);
 		frame.getContentPane().add(lblCityStZip);
-		
-		textField_2 = new JTextField();
-		textField_2.setBounds(182, 234, 176, 20);
-		frame.getContentPane().add(textField_2);
-		textField_2.setColumns(10);
-		
-		textField_3 = new JTextField();
-		textField_3.setBounds(182, 265, 176, 20);
-		frame.getContentPane().add(textField_3);
-		textField_3.setColumns(10);
-		
+
+		companyCityStateZip = new JTextField();
+		companyCityStateZip.setName("companyCityStateZip");
+		companyCityStateZip.setBounds(163, 174, 176, 20);
+		frame.getContentPane().add(companyCityStateZip);
+		companyCityStateZip.setColumns(10);
+
+		companyPhoneNumber = new JTextField();
+		companyPhoneNumber.setName("companyPhoneNumber");
+		companyPhoneNumber.setBounds(163, 199, 176, 20);
+		frame.getContentPane().add(companyPhoneNumber);
+		companyPhoneNumber.setColumns(10);
+
 		JLabel lblNewLabel_1 = new JLabel("Phone Number:");
-		lblNewLabel_1.setBounds(28, 268, 144, 14);
+		lblNewLabel_1.setBounds(28, 202, 144, 14);
 		frame.getContentPane().add(lblNewLabel_1);
-		
+
 		JLabel lblInvoiceNumber = new JLabel("Invoice Number: ");
-		lblInvoiceNumber.setBounds(28, 353, 144, 14);
+		lblInvoiceNumber.setBounds(28, 281, 144, 14);
 		frame.getContentPane().add(lblInvoiceNumber);
-		
-		textField_4 = new JTextField();
-		textField_4.setBounds(182, 350, 176, 20);
-		frame.getContentPane().add(textField_4);
-		textField_4.setColumns(10);
-		
+
+		invoiceNumber = new JTextField();
+		invoiceNumber.setName("invoiceNumber");
+		invoiceNumber.setBounds(163, 278, 176, 20);
+		frame.getContentPane().add(invoiceNumber);
+		invoiceNumber.setColumns(10);
+
 		JLabel lblDateInvoicePayable = new JLabel("Date Invoice Payable:");
-		lblDateInvoicePayable.setBounds(28, 402, 154, 14);
+		lblDateInvoicePayable.setBounds(28, 306, 154, 14);
 		frame.getContentPane().add(lblDateInvoicePayable);
-		
-		textField_5 = new JTextField();
-		textField_5.setBounds(182, 399, 176, 20);
-		frame.getContentPane().add(textField_5);
-		textField_5.setColumns(10);
+
+		invoicePayableDate = new JTextField();
+		invoicePayableDate.setName("invoicePayableDate");
+		invoicePayableDate.setBounds(163, 309, 176, 20);
+		frame.getContentPane().add(invoicePayableDate);
+		invoicePayableDate.setColumns(10);
+
+		JLabel lblCompanyInformation = new JLabel("Your Company Information");
+		lblCompanyInformation.setFont(new Font("Tahoma", Font.BOLD | Font.ITALIC, 15));
+		lblCompanyInformation.setBounds(28, 85, 267, 14);
+		frame.getContentPane().add(lblCompanyInformation);
+
+		JLabel lblNewLabel_2 = new JLabel("Invoice Information");
+		lblNewLabel_2.setFont(new Font("Tahoma", Font.BOLD | Font.ITALIC, 15));
+		lblNewLabel_2.setBounds(28, 256, 222, 14);
+		frame.getContentPane().add(lblNewLabel_2);
+
+		JLabel lblYourCompanyName = new JLabel("Your company name: ");
+		lblYourCompanyName.setBounds(28, 118, 144, 14);
+		frame.getContentPane().add(lblYourCompanyName);
+
+		JLabel invoiceGenerated = new JLabel("Josh Harm copyright 2019");
+		invoiceGenerated.setHorizontalAlignment(SwingConstants.CENTER);
+		invoiceGenerated.setBounds(-10, 654, 388, 14);
+		frame.getContentPane().add(invoiceGenerated);
+
+		JLabel lblCustomerInformation = new JLabel("Customer Information");
+		lblCustomerInformation.setFont(new Font("Tahoma", Font.BOLD | Font.ITALIC, 15));
+		lblCustomerInformation.setBounds(28, 360, 222, 14);
+		frame.getContentPane().add(lblCustomerInformation);
+
+		JLabel lblCustomerFullName = new JLabel("Full name: ");
+		lblCustomerFullName.setBounds(28, 396, 144, 14);
+		frame.getContentPane().add(lblCustomerFullName);
+
+		JLabel label = new JLabel("Street Address: ");
+		label.setBounds(28, 421, 144, 14);
+		frame.getContentPane().add(label);
+
+		JLabel label_1 = new JLabel("City, ST ZIP Code: ");
+		label_1.setBounds(28, 446, 154, 14);
+		frame.getContentPane().add(label_1);
+
+		JLabel label_2 = new JLabel("Phone Number:");
+		label_2.setBounds(28, 471, 144, 14);
+		frame.getContentPane().add(label_2);
+
+		customerFullName = new JTextField();
+		customerFullName.setName("customerFullName");
+		customerFullName.setColumns(10);
+		customerFullName.setBounds(163, 393, 176, 20);
+		frame.getContentPane().add(customerFullName);
+
+		customerStreetAddress = new JTextField();
+		customerStreetAddress.setName("customerStreetAddress");
+		customerStreetAddress.setColumns(10);
+		customerStreetAddress.setBounds(163, 418, 176, 20);
+		frame.getContentPane().add(customerStreetAddress);
+
+		customerCityStateZip = new JTextField();
+		customerCityStateZip.setName("customerCityStateZip");
+		customerCityStateZip.setColumns(10);
+		customerCityStateZip.setBounds(163, 443, 176, 20);
+		frame.getContentPane().add(customerCityStateZip);
+
+		customerPhoneNumber = new JTextField();
+		customerPhoneNumber.setName("customerPhoneNumber");
+		customerPhoneNumber.setColumns(10);
+		customerPhoneNumber.setBounds(163, 468, 176, 20);
+		frame.getContentPane().add(customerPhoneNumber);
+
+		JLabel notNullLabel = new JLabel("");
+		notNullLabel.setEnabled(false);
+		notNullLabel.setBounds(47, 521, 311, 14);
+		frame.getContentPane().add(notNullLabel);
+
+		JButton createPDFButton = new JButton("Create PDF Invoice");
+		createPDFButton.setBounds(28, 619, 303, 23);
+
+		createPDFButton.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				validateSubmission();
+			}
+
+			private void validateSubmission() {
+				try {
+					notNull(companyName);
+					notNull(companyStreetAddress);
+					notNull(companyCityStateZip);
+					notNull(companyPhoneNumber);
+					
+					notNull(invoiceNumber);
+					notNull(invoicePayableDate);
+
+					notNull(customerFullName);
+					notNull(customerStreetAddress);
+					notNull(customerStreetAddress);
+					notNull(customerCityStateZip);
+					notNull(customerPhoneNumber);
+
+					notNullLabel.setEnabled(false);
+					notNullLabel.setText("");
+					
+				} catch (ProcessingException e) {
+					notNullLabel.setEnabled(true);
+					notNullLabel.setText("The following field cannot be empty: " + e.getMessage());
+					notNullLabel.setBackground(Color.black);
+					notNullLabel.setForeground(Color.RED);
+				}
+
+			}
+
+			private void notNull(JTextField field) {
+
+				if (null == field.getText() || "".equals(field.getText())) {
+					throw new ProcessingException(field.getName());
+				}
+
+			}
+
+		});
+
+		frame.getContentPane().add(createPDFButton);
 
 	}
 }
---
+++ src/src/com/harm/solutions/utilities/ProcessingException.java
@@ -0,0 +1,9 @@
+package src.com.harm.solutions.utilities;
+
+public class ProcessingException extends RuntimeException {
+
+	public ProcessingException(String string) {
+		super(string);
+	}
+
+}
